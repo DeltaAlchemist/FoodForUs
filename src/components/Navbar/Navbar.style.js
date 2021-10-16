09b10cf876dbfc37eeae26a1dac1c19456fef4d1
Henrique Campos <delta.alchemist@gmail.com>
2021-10-16 16:05:39 -0300

    feat: basic site structure

---
+++ src/components/Navbar/Navbar.style.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles"
+
+export default makeStyles(theme => ({
+    menuButton: {
+        marginRight: 2,
+    },
+    title: {
+        flexGrow: 1,
+    },
+}))
---
+++ src/layout/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+
+export const Layout = () => {
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
