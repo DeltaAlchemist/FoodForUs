aeb07bdbf794c183344d54735fca5f138af1921e
Henrique Campos <delta.alchemist@gmail.com>
2021-10-16 21:24:18 -0300

    feat: add top menu/navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react'
 
-import ModalDialog from './ModalDialog';
 import useStyles from './Navbar.style';
 
-const Navbar = () => {
+export const Navbar = () => {
 
     return(
-        <> 
-        </>
+        <div className="ui large top fixed hidden menu"> 
+            <div className="ui container">
+                <a className="active item">Home</a>
+                <a className="item">Symbot</a>
+                <a className="item">Artigos</a>
+                <a className="item">Contato</a>
+                <a className="item">Sobre</a>
+            </div>
+            <div className="right menu">
+                <div className="item">
+                    <a className="ui button">Log in</a>
+                </div>
+                <div className="item">
+                    <a className="ui primary button">Registrar</a>
+                </div>
+            </div>
+        </div>
     );
 }
-
-export default Navbar
