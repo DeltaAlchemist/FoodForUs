f82c0102df4897defac74e8f6de9b86231f8000b
lucasbado <luc.bado@gmail.com>
2021-10-22 21:36:43 -0300

    Raposym integrado

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Artigos from './pages/Artigos';
 import Contato from './pages/Contato';
 import Sobre from './pages/Sobre';
+import Raposym from './pages/Raposym';
 import { Footer } from './components/Footer/Footer';
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
                 <Route path="/artigos" component={Artigos} />
                 <Route path="/contato" component={Contato} />
                 <Route path="/sobre" component={Sobre} />
+                <Route path="/raposym" component={Raposym} />
             </Switch>
             <Footer/>
         </BrowserRouter>  
